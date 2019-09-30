8d04b3bff6aed83e021c344eaef239d30e9a5f60
dale.torre <dale.torre@ubiquitygs.com>
2019-09-30 18:14:43 -0400

    Working on description text editor

--- resources/views.py
+++ resources/views.py
@@ -4,25 +4,31 @@ from django.shortcuts 		import render
 from django.shortcuts 		import redirect
 
 from . viewmodels 			import ResourcesViewModel
+from category.viewmodels 	import CategoryViewModel
 
 
 resources = ResourcesViewModel()
+categories = CategoryViewModel()
 
 
 def blog(request):
 
 	blog_list = resources.get_blog_list()
+	category_list = categories.get_category_list()
 
 	return render(request, 'resources/blog.html',
-				 {'blog_list':blog_list})
+				 {'blog_list':blog_list,
+				  'category_list':category_list})
 
 
 def blog_detail(request, blogId):
 
 	blog = resources.get_blog_by_id(blogId)
+	category_list = categories.get_category_list()
 
 	return render(request, 'resources/blog_detail.html',
-				 {'blog':blog})
+				 {'blog':blog,
+				  'category_list':category_list})
 
 
 def privacy(request):
