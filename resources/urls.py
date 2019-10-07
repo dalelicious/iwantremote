1b122466b076419760c3bec6cf9e2abc4ace29c0
Denmark Contrevida <denmark.contrevida@corp.ubiquitygs.com>
2019-10-07 11:55:46 -0400

    update css

--- resources/urls.py
+++ resources/urls.py
@@ -7,5 +7,6 @@ app_name = 'resources'
 urlpatterns = [
 	path('blog/', views.blog, name='blog'),
 	path('blog_detail/<int:blogId>/', views.blog_detail, name='blog-detail'),
-	path('privacy/', views.privacy, name='privacy')
+	path('privacy/', views.privacy, name='privacy'),
+	path('job_template/', views.privacy, name='job_template')
 ]
