bdbed113d92c5c4a2664b6ba0e918df8fcb138db
denmarkcontrevida2017 <denmarkcontrevida@gmail.com>
2019-10-10 20:42:42 +0800

    fix

--- resources/urls.py
+++ resources/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
 	path('blog/', views.blog, name='blog'),
 	path('blog_detail/<int:blogId>/', views.blog_detail, name='blog-detail'),
 	path('privacy/', views.privacy, name='privacy'),
-	path('job_template/', views.privacy, name='job_template')
+	path('job_template/', views.job_template, name='job_template')
 ]
