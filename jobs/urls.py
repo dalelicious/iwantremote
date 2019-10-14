ed99aa5bb7dc8e06668d8403b45c2682b8beeaa1
Denmark Contrevida <denmark.contrevida@corp.ubiquitygs.com>
2019-10-14 08:30:20 -0400

    change URL in jobs

--- jobs/urls.py
+++ jobs/urls.py
@@ -7,5 +7,5 @@ app_name = 'jobs'
 urlpatterns = [
     path('jobs/', 						views.jobs, 		name='jobs'),
     path('create_job/', 				views.create_job, 	name='create-job'),
-    path('job_detail/<slug:jobName>/', 	views.job_detail, 	name='job-detail')
+    path('remote-jobs/<slug:jobName>/', 	views.job_detail, 	name='job-detail')
 ]
