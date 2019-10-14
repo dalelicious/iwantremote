86eb6495e87df5cd5757d00da06d5da17d0b8a4f
denmarkcontrevida2017 <denmarkcontrevida@gmail.com>
2019-10-15 01:14:05 +0800

    fix sitemap

--- jobs/sitemaps.py
+++ jobs/sitemaps.py
@@ -2,6 +2,11 @@ from django.contrib.sitemaps import Sitemap
 from . models import Jobs
 
 class PostSitemap(Sitemap):
+	changefreq = "hourly"
+	priority = 1.0
 
 	def items(self):
-		return Jobs.objects.all().order_by('id')
+		return Jobs.objects.all().order_by
+
+	def lastmod(self, obj):
+		return obj.create_date
