34c5d2a4a14abbd0aedd9ad27a604f69ef91bbcb
Denmark Contrevida <denmark.contrevida@corp.ubiquitygs.com>
2019-10-15 09:15:19 -0400

    update sitemap

--- jobs/sitemaps.py
+++ jobs/sitemaps.py
@@ -6,7 +6,7 @@ class PostSitemap(Sitemap):
 	priority = 1.0
 
 	def items(self):
-		return Jobs.objects.all().order_by
+		return Jobs.objects.all().order_by('id')
 
 	def lastmod(self, obj):
 		return obj.create_date
