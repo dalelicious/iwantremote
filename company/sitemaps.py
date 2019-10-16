066d1d2503ff3c98a9474c32bdf1fbf3db36dad7
Denmark Contrevida <denmark.contrevida@corp.ubiquitygs.com>
2019-10-16 08:21:56 -0400

    update sitemaps

--- company/sitemaps.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.sitemaps import Sitemap
-from . models import Company
-
-class CompanySitemap(Sitemap):
-	changefreq = "hourly"
-	priority = 1.0
-
-	def items(self):
-		return ['company']
