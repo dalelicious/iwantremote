fabf9d44c33e4f7a740830f077017be2a757c63a
Denmark Contrevida <denmark.contrevida@corp.ubiquitygs.com>
2019-10-17 04:38:02 -0400

    add robots.txt

--- iwantremote/urls.py
+++ iwantremote/urls.py
@@ -4,6 +4,7 @@ from django.urls 				import path
 from django.urls 				import include
 from django.contrib 			import admin
 from django.conf.urls.static 	import static
+from django.views.generic import TemplateView
 
 # Sitemap
 from django.contrib.sitemaps.views import sitemap
@@ -26,5 +27,6 @@ urlpatterns = [
 		name='django.contrib.sitemaps.views.sitemap'),
 	path('remote-jobs/<slug:jobName>', views.post, name='jobpost'),
 	path('admin/',      admin.site.urls),
+	path('robots.txt', TemplateView.as_view(template_name="jobs/robots.txt", content_type='text/plain')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
