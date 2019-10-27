0ffd87f86c749e8375559d931e7773422a12b6e7
dale.torre <dale.torre@ubiquitygs.com>
2019-10-27 09:52:55 -0400

    Show job posted in subscriber success page

--- subscriber/views.py
+++ subscriber/views.py
@@ -4,19 +4,23 @@ from django.shortcuts 		import render
 from django.shortcuts 		import redirect
 
 from . viewmodels 			import SubscriberViewModel
+from jobs.viewmodels 		import JobsViewModel
 from category.viewmodels 	import CategoryViewModel
 
 
+job = JobsViewModel()
 subscribers = SubscriberViewModel()
 categories = CategoryViewModel()
 
 
 def confirmation(request):
 
+	all_jobs = len(job.get_jobs_list())
 	category_list = categories.get_category_list()
 
 	return render(request, 'subscriber/confirmation.html',
-				 {'category_list':category_list})
+				 {'all_jobs':all_jobs,
+				  'category_list':category_list})
 
 def add_subscriber(request):
 
