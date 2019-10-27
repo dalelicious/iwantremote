b4ce7f017383323e620b88704adfcc859d7ee1cf
dale.torre <dale.torre@ubiquitygs.com>
2019-10-27 10:07:18 -0400

    Limit post per category to 15

--- category/models.py
+++ category/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
 	@property
 	def jobs_per_category(self):
 
-		jobs_per_category = Jobs.objects.filter(category=self.slugCatName, is_active=True).order_by('-create_date')[:3]
+		jobs_per_category = Jobs.objects.filter(category=self.slugCatName, is_active=True).order_by('-create_date')[:15]
 
 		return jobs_per_category
 
