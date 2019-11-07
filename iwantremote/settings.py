ac9c1c8ea0eb3fde4ca32bd5738d042bdffdcf33
dale.torre <dale.torre@ubiquitygs.com>
2019-11-07 07:35:16 -0500

    Updates for beta testing

--- iwantremote/settings.py
+++ iwantremote/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '7#*qk6(i=4_ur+q%&@o@ac@79htj4f_kq)!6&zjvm-s0th2ae@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
